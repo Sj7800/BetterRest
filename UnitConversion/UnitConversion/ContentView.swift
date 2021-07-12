0931698cc87f5f69a017bab9b589b03b97b745df
Swapanjeet <swapanjeet.sumeru@outlook.com>
2021-07-13 01:53:07 +0530

    Unit Conversion with Swift UI

---
+++ UnitConversion/UnitConversion/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Swapanjeet Singh on 05/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @ State private var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            //Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+           
+            VStack(spacing: 30) {
+                Text("Tap the flag of")
+                    .foregroundColor(.white)
+                Text(countries[correctAnswer])
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            
+            ForEach(0 ..< 3) { number in
+                Button(action: {
+                    // Flag was tapped
+                    self.flagTapped(number)
+                }) {
+                    
+                    Image(self.countries[number])
+                        .renderingMode(.original)
+                        .clipShape(Capsule())
+                        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                        .shadow(color: .black, radius: 2)
+                        
+                }
+                
+            }
+            
+        
+            
+        
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+}
--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    @State private var totalCheckAmount = ""
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        //  let totalAmount = Double(totalCheckAmount)
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Check Amount")) {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Total Amount for check")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                }
+                
+                Picker("Number of People", selection: $numberOfPeople) {
+                            ForEach(2 ..< 100) {
+                                    Text("\($0) people")
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("We Split")
+        }
+        
     }
 }
 
+// To show a preview of your UI design alongside
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
